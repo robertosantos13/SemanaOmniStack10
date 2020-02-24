bf15aedfb2dc7c163db56c5212cab0d4644b661a
osrbeto <osrbeto@hotmail.com>
2020-02-23 22:55:48 -0300

    implementando o backend com as rotas principais,index, store,search, update e delete, models e controllers

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,79 @@
+const Dev = require('../models/Dev');
+const axios = require('axios');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index(request, response) {
+        const dev = await Dev.find();
+        return response.json(dev);
+    },
+    async store(request, response) {
+        const { github_usename, techs, latitude, longitude } = request.body;
+
+        let dev = await Dev.findOne({ github_usename });
+
+        if (!dev) {
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_usename}`);
+
+            const { name = login, avatar_url, bio } = apiResponse.data;
+
+            const techsArray = parseStringAsArray(techs);
+
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            }
+
+            dev = await Dev.create({
+                github_usename,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            })
+        }
+
+        return response.json(dev);
+
+    },
+
+    async update(request, response) {
+        const { id } = request.query;
+
+        const { techs, name, latitude, longitude } = request.body;
+
+        const techsArray = parseStringAsArray(techs);
+
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+        }
+
+        const dev = await Dev.updateOne({ _id: id }, {
+            name,
+            techs: techsArray,
+            location
+        })
+
+
+        if (dev.n == 1) {
+            return response.json({ 'Update': true });
+        }
+
+        return response.json({ dev: dev });
+
+    },
+
+    async destroy(request, response) {
+        const { id } = request.query;
+
+        const dev = await Dev.deleteOne({ _id: id });
+
+        if (dev.n == 1) {
+            return response.json({ 'Delete': true });
+        }
+
+        return response.json({ dev: [] });
+    }
+}
---
+++ backend/src/routes.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
+const routes = Router();
+
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store)
+routes.put('/update', DevController.update);
+routes.delete('/delete', DevController.destroy);
+
+
+routes.get('/search', SearchController.index);
+
+
+
+module.exports = routes;
